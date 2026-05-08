899b2da11003d5d50444af814538bf95173586b9
樱鸣流雨 <a692256293@163.com>
2026-05-08 11:29:42 +0800

    feat(homepage): 初始化首页

---
+++ src/view/homepage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="main-wrap">
+
+  </div>
+</template>
+
+<style scoped>
+.main-wrap {
+  width: 100%;
+  height: 100vh;
+  background-color: var(--color-bg);
+}
+</style>

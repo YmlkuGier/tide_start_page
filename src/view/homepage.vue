0a7b1fa235c591ba488703886249d96b9c3d7c18
樱鸣流雨 <a692256293@163.com>
2026-05-13 10:50:30 +0800

    feat(homepage): 设置设置按钮

--- src/view/homepage.vue
+++ src/view/homepage.vue
@@ -7,6 +7,9 @@ import SearchBar from "../components/searchBar.vue";
   <div class="main-wrap">
     <search-bar />
   </div>
+  <div class="setting-wrap">
+    <img src="../assets/setting.svg" alt="">
+  </div>
 </template>
 
 <style scoped>
@@ -17,5 +20,20 @@ import SearchBar from "../components/searchBar.vue";
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.setting-wrap {
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  position: fixed;
+  bottom: 20px;
+  left: 20px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.setting-wrap:hover {
+  background-color: rgba(0, 0, 0, 0.1);
 }
 </style>

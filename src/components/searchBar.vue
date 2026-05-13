c98410b76a1de39813c3e95c508522938447b851
樱鸣流雨 <a692256293@163.com>
2026-05-13 09:27:39 +0800

    feat(homepage&searchBar): 完成基本的搜索栏

---
+++ src/components/searchBar.vue
@@ -0,0 +1,128 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const item = ref([
+  {
+    id: 'baidu',
+    name: '百度',
+    url: 'https://www.baidu.com/s?wd=',
+    icon: 'https://www.baidu.com/favicon.ico'
+  },
+  {
+    id: 'bing',
+    name: '必应',
+    url: 'https://www.bing.com/search?q=',
+    icon: 'https://www.bing.com/sa/simg/favicon-trans-bg-blue-mg-28.ico'
+  },
+  {
+    id: 'google',
+    name: 'Google',
+    url: 'https://www.google.com/search?q=',
+    icon: 'https://www.google.com/favicon.ico'
+  }
+])
+
+</script>
+
+<template>
+  <div class="searchBar_wrap">
+    <div class="searchBar">
+      <div class="icon_wrap">
+        <img src="../assets/search.svg" alt="">
+      </div>
+      <input id="input">
+    </div>
+    <div class="option_wrap">
+      <div class="opt" v-for="item in item" :key="item.id">
+        <img :src="item.icon" alt="">
+        <span>{{item.name}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.searchBar_wrap {
+  width: auto;
+  height: auto;
+  border-radius: 25px;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+}
+.searchBar {
+  height: 50px;
+  width: 600px;
+  border-radius: 25px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  img {
+    width: 24px;
+    margin-left: 10px;
+    margin-right: 5px;
+  }
+}
+.icon_wrap {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#input {
+  height: 100%;
+  width: 400px;
+  padding: 0;
+  font-size: 16px;
+  font-weight: normal;
+  -webkit-text-stroke: 0.5px transparent;
+  border: none;
+  outline: none;
+  box-shadow: none;
+}
+.option_wrap {
+  min-width: 150px;
+  max-width: 200px;
+  width: fit-content;
+  height: auto;
+  border-radius: 15px;
+  background-color: white;
+  padding: 0 5px;
+  margin-left: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 5px;
+}
+.opt {
+  border-radius: 15px;
+  width: 100%;
+  white-space: nowrap;
+  height: 40px;
+  padding: 0 10px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 5px;
+  img {
+    width: 30px;
+    flex-shrink: 0
+  }
+  span {
+    font-size: 16px;
+    max-width: calc(100% - 40px);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+.opt:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+</style>
--- src/view/homepage.vue
+++ src/view/homepage.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import SearchBar from "../components/searchBar.vue";
 
 </script>
 
 <template>
   <div class="main-wrap">
-
+    <search-bar />
   </div>
 </template>
 
@@ -13,5 +14,8 @@
   width: 100%;
   height: 100vh;
   background-color: var(--color-bg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>

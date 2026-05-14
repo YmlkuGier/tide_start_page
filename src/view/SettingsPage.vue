1f293cb7ed1e592c00029cc7a311948fe3a30361
樱鸣流雨 <a692256293@163.com>
2026-05-14 15:03:30 +0800

    feat(SettingsPage): 完成设置界面基础布局

--- src/view/SettingsPage.vue
+++ src/view/SettingsPage.vue
@@ -3,9 +3,25 @@
 </script>
 
 <template>
-<h1>设置页面</h1>
+  <div class="setting-page-wrap">
+    <div id="left-menu"></div>
+    <div id="setting-main-wrap"></div>
+  </div>
 </template>
 
 <style scoped>
-
+.setting-page-wrap {
+  width: 100%;
+  height: 100vh;
+  background-color: var(--color-settings-bg);
+  display: flex;
+}
+#left-menu {
+  width: 250px;
+  height: 100%;
+  background-color: var(--color-settings-menu-bg);
+}
+#setting-main-wrap {
+  flex: 1;
+}
 </style>
